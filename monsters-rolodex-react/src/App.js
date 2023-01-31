d3ef9b0cedcabfc50207d0e6f04306761f7dfd94
ButterBrot777 <korendos@gmail.com>
2023-01-31 16:32:57 +0300

    feat: add search box component

--- monsters-rolodex-react/src/App.js
+++ monsters-rolodex-react/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -13,6 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    console.log('did mount')
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -39,12 +41,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="cearch monsters"
-          onChange={onSearchChange}
-        ></input>
+        <SearchBox
+          className={'search-box'}
+          onChangeHandler={onSearchChange}
+          placeholder={'search monsters'}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
